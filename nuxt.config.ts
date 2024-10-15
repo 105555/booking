7d0fc35762e261fbeb1bcd693396dad403922766
105555 <fter2257@gmail.com>
2024-10-15 12:23:24 +0800

    feat: 新增初始配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src', 
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  typescript: {
+    strict: true
+  },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss'
+  ],
+  colorMode: {
+    classSuffix: '',
+  },
+  i18n: {
+    locales: ['en', 'zhcn'],
+    defaultLocale: 'zhcn'
+  },
+  css: [
+    '@/assets/css/tailwind.css',  
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  pages: true,
+  components: true,
 })
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,2 @@
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
 }
